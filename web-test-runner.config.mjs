0a9df6b17e88beff60332329045d8098c8f2295b
Meghea Iulian <iulian.meghea@gmail.com>
2024-03-17 22:10:57 +0200

    Collect code coverage

--- web-test-runner.config.mjs
+++ web-test-runner.config.mjs
@@ -1,5 +1,15 @@
 import { esbuildPlugin } from "@web/dev-server-esbuild";
 
 export default {
+  coverageConfig: {
+    reportDir: "coverage",
+    threshold: {
+      statements: 70,
+      branches: 70,
+      functions: 50,
+      lines: 70,
+    },
+  },
+  nodeResolve: true,
   plugins: [esbuildPlugin({ ts: true })],
 };
