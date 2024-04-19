21cfaf8cf0aa9fb29913870724e44c048c091d95
Meghea Iulian <iulian.meghea@gmail.com>
2024-04-19 09:10:00 +0300

    Test updating cem

--- custom-elements-manifest.config.js
+++ custom-elements-manifest.config.js
@@ -1,4 +1,27 @@
-import { jsdocFunctionPlugin } from "cem-plugin-jsdoc-function";
+/**
+ * @return {import('@custom-elements-manifest/analyzer').Plugin}
+ */
+function jsdocFunctionPlugin() {
+  return {
+    name: "jsdoc-function",
+    analyzePhase({ ts, node, moduleDoc }) {
+      if (ts.isVariableStatement(node)) {
+        // @ts-expect-error: it very well might
+        const comments = node.jsDoc ?? [];
+        const tags = comments.flatMap((x) => x.tags ?? []);
+        if (tags.some((x) => x.tagName?.getText?.() === "function")) {
+          const { declarations } = node.declarationList;
+          if (declarations.length > 1) return;
+          const [{ name }] = declarations;
+          const doc = moduleDoc.declarations.find(
+            (d) => d.name === name.getText()
+          );
+          doc.kind = "function";
+        }
+      }
+    },
+  };
+}
 
 /** @type {{ plugins: import('@custom-elements-manifest/analyzer').Plugin[] }} */
 export default {
